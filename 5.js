036a7bde7e1215be658273f5278399a8be5f0e3c
nicolf17 <nicolaslopezforero17@gmail.com>
2025-02-24 09:14:23 -0500

    Actualizar

--- 5.js
+++ 5.js
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-function pedirNota(materia){
-    let pedirNota
-    do{
-        nota= Number(prompt(`Ingrese su calificacion de ${materia} (0-10): `))
-    } while (isNaN(nota) || nota<0 || nota>10)
-        return nota
-}
-
-let fisica= pedirNota("fisica")
-let quimica= pedirNota("quimica")
-let biologia= pedirNota("biologia")
-let matematicas= pedirNota("matematicas")
-let informatica= pedirNota("inofrmatica")
-
-let calificacion
-let porcentaje= ((fisica+quimica+biologia+matematicas+informatica)/50)*100
-
-if (porcentaje > 80) {
-    calificacion = "Excelente"
-} else if (porcentaje > 60) {
-    calificacion = "Buena"
-} else if (porcentaje > 0) {
-    calificacion = "Mala"
-} else {
-    calificacion = "Inválida"
-}
-
-
-alert(`Tu porcentaje es del ${porcentaje}% y tu calificacion es ${calificacion}`)
-=======
 function pedirNota(materia) {
     let pedirNota
     do {
@@ -65,4 +34,4 @@ switch (true) {
 }
 
 alert(mensaje)
->>>>>>> fcbdcb9 (Actualizar)
+
