18dcaebb2e755aec2d4ec2a04ff5603c98833b05
nicolf17 <nicolaslopezforero17@gmail.com>
2025-02-24 09:09:02 -0500

    Actualizar

--- 5.js
+++ 5.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function pedirNota(materia){
     let pedirNota
     do{
@@ -27,3 +28,41 @@ if (porcentaje > 80) {
 
 
 alert(`Tu porcentaje es del ${porcentaje}% y tu calificacion es ${calificacion}`)
+=======
+function pedirNota(materia) {
+    let pedirNota
+    do {
+        nota = Number(prompt(`Ingrese su calificacion de ${materia} (0-10): `))
+    } while (isNaN(nota) || nota < 0 || nota > 10)
+    return nota
+}
+
+let fisica = pedirNota("fisica")
+let quimica = pedirNota("quimica")
+let biologia = pedirNota("biologia")
+let matematicas = pedirNota("matematicas")
+let informatica = pedirNota("inofrmatica")
+
+let calificacion
+let porcentaje = ((fisica + quimica + biologia + matematicas + informatica) / 50) * 100
+
+switch (true) {
+    case (porcentaje > 80):
+        calificacion = ("excelente")
+        mensaje = (`Tu porcentaje es del ${porcentaje}% y tu calificacion es ${calificacion}`)
+        break
+    case (porcentaje > 60):
+        calificacion = ("buena")
+        mensaje = (`Tu porcentaje es del ${porcentaje}% y tu calificacion es ${calificacion}`)
+        break
+    case (porcentaje > 0 && porcentaje < 60):
+        calificacion = ("mala")
+        mensaje = (`Tu porcentaje es del ${porcentaje}% y tu calificacion es ${calificacion}`)
+        break
+    default:
+        calificacion = ("Invalido")
+        mensaje = (`Tu porcentaje es del ${porcentaje}% y tu calificacion es ${calificacion}`)
+}
+
+alert(mensaje)
+>>>>>>> fcbdcb9 (Actualizar)
